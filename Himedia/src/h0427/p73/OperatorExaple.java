411751c497359e6c72bc02892cbd8a8996f06afc
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 15:03:28 +0900

    20230427수정

--- Himedia/src/h0427/p73/OperatorExaple.java
+++ Himedia/src/h0427/p73/OperatorExaple.java
@@ -3,7 +3,23 @@ package h0427.p73;
 public class OperatorExaple {
 
 	public static void main(String[] args) {
+		byte a =10;
+		int b = 5;
+		int c = 7;
+		double d = 4.8;
 		
+		int result1 = a + b;
+		double result2 = c + d;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		
+//		abc
 	}
 
 }
