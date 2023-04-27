20a9b25a88d5a192527d6991367f6d886bd72e4e
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 16:31:39 +0900

    p75~78

---
+++ Himedia/src/h0427/p78/AccuracyExample.java
@@ -0,0 +1,16 @@
+package h0427.p78;
+
+public class AccuracyExample {
+
+	public static void main(String[] args) {
+		int apple = 1;
+		double pieceUnit = 0.1;
+		int number = 7;
+		
+//		          ?   =  1    -   7    *   0.1
+		double result = apple - number * pieceUnit;
+		
+		System.out.println(result);
+	}
+
+}
---
+++ Himedia/src/h0427/p79/AccuracyExample.java
@@ -0,0 +1,16 @@
+package h0427.p79;
+
+public class AccuracyExample {
+
+	public static void main(String[] args) {
+		int apple = 1;
+		int totalPieces = apple * 10;
+		int number = 7;
+		
+		int temp = totalPieces - number;
+		double result = temp / 10.0;
+		
+		System.out.println(result);
+	}
+
+}
