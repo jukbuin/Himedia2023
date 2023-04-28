6faa5dd2e0510648a9ab0492bc0ba1699b0f0a45
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 18:18:24 +0900

    번외문제~

---
+++ Himedia/src/h0428q/Main.java
@@ -0,0 +1,14 @@
+package h0428q;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int i = 1; i < 16; i++) {
+			System.out.print(i);
+		if (i < 15) {
+				System.out.print("?");
+			}
+		} 
+	}
+
+}
