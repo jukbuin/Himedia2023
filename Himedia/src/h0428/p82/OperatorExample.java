17fe9ec043827148ea75a4c4b28387d3423e3f33
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 10:31:02 +0900

    p82

---
+++ Himedia/src/h0428/p82/OperatorExample.java
@@ -0,0 +1,16 @@
+package h0428.p82;
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		int x = 5;
+		double y = 0.0;
+		
+		double z = x/y;
+		
+		System.out.println(Double.isInfinite(z));
+		System.out.println(Double.isNaN(z));
+		System.out.println(z + 2);
+	}
+
+}
