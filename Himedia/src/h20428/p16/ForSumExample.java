d6e9cfb3b1ff39dfea3aae3390c25af85132abf5
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 16:32:54 +0900

    2.p16

---
+++ Himedia/src/h20428/p16/ForSumExample.java
@@ -0,0 +1,19 @@
+package h20428.p16;
+
+import java.util.Scanner;
+
+public class ForSumExample {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int inp = sc.nextInt();
+		int sum = 0;
+		sc.close();
+		
+		for(int i =1; i <= inp; i++) {
+			sum += i;
+		}
+		System.out.println("sum = " + sum);
+	}
+
+}
