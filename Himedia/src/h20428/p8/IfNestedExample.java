4a3b38108f17994d53b6be4d2a6b80e23cc4b815
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 14:46:45 +0900

    2.p8수정

--- Himedia/src/h20428/p8/IfNestedExample.java
+++ Himedia/src/h20428/p8/IfNestedExample.java
@@ -1,9 +1,14 @@
 package h20428.p8;
 
+import java.util.Scanner;
+
 public class IfNestedExample {
 
 	public static void main(String[] args) {
-		int score = 84;
+		Scanner scan = new Scanner(System.in);
+//		int score = 84;
+		int score = scan.nextInt();
+		scan.close();
 
 		if (score >= 90) {
 			System.out.print("A");
