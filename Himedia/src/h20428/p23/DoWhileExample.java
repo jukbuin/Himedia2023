1c4995f2b82aeb5c57c92f9b0538a50e3a3a9746
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 16:59:44 +0900

    2.p23

---
+++ Himedia/src/h20428/p23/DoWhileExample.java
@@ -0,0 +1,13 @@
+package h20428.p23;
+
+public class DoWhileExample {
+
+	public static void main(String[] args) {
+		int i = 1;
+		
+		do {
+			System.out.println(i++);
+		} while (i <= 5);
+	}
+
+}
