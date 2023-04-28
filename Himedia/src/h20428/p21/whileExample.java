f89f0c8e93b7c77f9fed9ced121119101f37bae6
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 16:56:07 +0900

    2.p21

---
+++ Himedia/src/h20428/p21/whileExample.java
@@ -0,0 +1,17 @@
+package h20428.p21;
+
+public class whileExample {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+
+			if (i == 5) {
+				break;
+			}
+		}
+	}
+
+}
