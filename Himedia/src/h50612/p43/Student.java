b1974396dcd02ff7385a96193814155cd1a642af
Class01 <Class01@DESKTOP-QKQPSM5>
2023-06-12 13:46:30 +0900

    java5 43페이지

---
+++ Himedia/src/h50612/p43/Student.java
@@ -0,0 +1,24 @@
+package h50612.p43;
+
+public class Student {
+	private int sno;
+	private String name;
+	
+	public Student(int sno, String name) {
+		this.sno = sno;
+		this.name = name;
+	}
+	
+	public boolean equlas(Object obj) {
+		if(obj instanceof Student) {
+			Student student = (Student) obj;
+			return (sno == student.sno) && (name.equals(student.name));
+		} else {
+			return false;
+		}
+	}
+	
+	public int hashCode() {
+		return sno + name.hashCode();
+	}
+}
